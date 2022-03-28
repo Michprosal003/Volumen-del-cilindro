4f4b77667177afda7f8b657f32063ff99a430514
Programador Michel Rosales <95501615+Michprosal003@users.noreply.github.com>
2022-03-28 03:17:11 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volumen_cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double  pi, radio, altura, resultado;
+
+            pi = 3.141592653589793;
+
+
+            Console.Title = "Volumen del cilindro";
+
+            Console.WriteLine(" Introduzca el valor del radio del cilindro, y su altura");
+
+            Console.WriteLine("Radio");
+            radio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Altura");
+            altura = double.Parse(Console.ReadLine());
+
+            resultado = (pi * radio * radio * altura);
+
+            Console.WriteLine(" La solucion es : {0}", resultado);
+
+
+            Console.ReadKey();
+        }
+    }
+}
